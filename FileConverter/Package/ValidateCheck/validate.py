3e4ab625fb80d572e12d8d3ab4ca155738b4b40f
김태형 <xo052345@gmail.com>
2021-10-05 23:08:13 +0900

    [김태형] Validate 클래스 구현

--- FileConverter/Package/ValidateCheck/validate.py
+++ FileConverter/Package/ValidateCheck/validate.py
@@ -1,13 +1,14 @@
 from Package.Directory.directory_func import *
 from Package.Debug.Error import *
 from Package.Debug.log_func import *
+import enum
 import openpyxl
 import yaml
 import glob
 import os
 
 class EConfigType(Enum):
-    excel : auto()
+    excel = auto()
 
 
 class Validate():
@@ -15,7 +16,8 @@ class Validate():
         self._targetSheetName = Get_ConfigFromJson(EConfigType.excel, 'targetSheetName')
         self._columnNameNum = Get_ConfigFromJson(EConfigType.excel, 'columnNameNum')
         self._keyColumnName = Get_ConfigFromJson(EConfigType.excel, 'keyColumnName')
-        self._targetExcelDataDic = {}
+        
+        self._allExcelDataDict = self.Init_DataListFromExcel()
     
 
     @deco_usedirmethod(EDirectory.excelDirectory)
@@ -27,16 +29,35 @@ class Validate():
             
             for sheetName in EXCEL_FILE.sheetnames:
                 if sheetName == targetSheetName:
-                    pass
+                    _dataDict = {}
+                    _rowNum = 0
+
+                    for rowData in EXCEL_FILE[sheetName].rows:
+                        _dataDict[_rowNum] = rowData
+                        _rowNum += 1
+
+                    return _dataDict
+
+            Write_Log(ELogTpye.warning, f'warning : no target sheet in [ {_excelName} ] excel data.')
+
+            return False
         
         _allExcelData = glob.glob('{0}*.xlsx'.format(exceDir))
+        _allExcelDataDict = {}
 
         for excelDir in _allExcelData:
-            
-            get_excelSheetDataDict(excelDir, self._targetSheetName)
-        pass
+            _excelName = str(os.path.basename(excelDir)).split('.')[0]
+            _excelDataDict = get_excelSheetDataDict(excelDir, self._targetSheetName)
 
+            if not _excelDataDict:
+                Write_Log(ELogTpye.warning, f'continue push [ {_excelName} ] excel data.')
+                continue
 
+            _allExcelDataDict[_excelName] = _excelDataDict
+
+        return _allExcelDataDict
+                
+        
 class NormalValidate(Validate):
     def __init__(self):
         super().__init__()
