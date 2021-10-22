9006c3bdbc8e3579587fbf4ff0c309a60947a5e7
김태형 <xo052345@gmail.com>
2021-10-22 23:35:57 +0900

    [김태형] Get_ConfigFromYaml 추가

--- FileConverter/Package/Config/config.py
+++ FileConverter/Package/Config/config.py
@@ -1,23 +1,38 @@
 from enum import Enum, auto
-import json
-import platform
 from Package.Directory.directory import EDirectory
 from Package.Directory.directory_func import deco_usedirmethod
+import json
+import yaml
 
 class EConfigType(Enum):
     excel = auto()
     data_id = auto()
 
 
+class EValidationConfigType(Enum):
+    ref_validation = auto()
+    value_size_compare = auto()
+
+
+def Get_ConfigTypeName(configType):
+    assert type(configType) == EConfigType or type(configType) == EValidationConfigType, 'err : wrong param data type input.'
+
+    _configEnum = (type(configType) == EConfigType) and EConfigType or EValidationConfigType
+
+    for member in _configEnum:
+        if configType == member:
+            return member._name_
+
+    assert (False), 'err : no config file value, matches enum member.'
+
+
 @deco_usedirmethod(EDirectory.toolConfigDirectory)
 def Get_ConfigFromJson(configType, configCategory, jsonPath):
     assert (type(configType) == EConfigType), 'err : wrong param data type input.'
-    if configType == EConfigType.excel:
-        _configTypeName = 'excel'
 
-    _jsonPath = jsonPath
+    _configTypeName = Get_ConfigTypeName(configType)
     
-    with open (_jsonPath) as file:
+    with open (jsonPath) as file:
         file_json = json.load(file)
         configSet = file_json[_configTypeName]
 
@@ -26,3 +41,16 @@ def Get_ConfigFromJson(configType, configCategory, jsonPath):
             return configSet[config]
 
     assert (False), 'err : input wrong config category.'
+
+
+@deco_usedirmethod(EDirectory.validationDirectory)
+def Get_ConifgFromYaml(configType, yamlPath):
+    assert (type(configType) == EValidationConfigType), 'err : wrong param data type input.'
+    
+    _configTypeName = Get_ConfigTypeName(configType)
+
+    with open(yamlPath) as file:
+        file_yaml = yaml.safe_load(file)
+        configSet = file_yaml[_configTypeName]
+
+    return configSet
