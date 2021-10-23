afdc74b1bdcf9992c132aad27edc0cf62ff97cc0
김태형 <xo052345@gmail.com>
2021-10-23 23:53:02 +0900

    [김태형] validationconfig 를 참조해 벨리데이트 체크를 하는 항목에서 공통으로 사용하는 데코레이터 추가

--- FileConverter/Package/ValidateCheck/validate_config.py
+++ FileConverter/Package/ValidateCheck/validate_config.py
@@ -1,21 +1,33 @@
 from Package.ValidateCheck.validate import *
 from Package.ValidateCheck.decorator import *
-import yaml
+from Package.Config.config import *
 
 class ConfigValidate(Validate):
+    """
+    Excel data validation with reference to vaildation config yaml file.
+    
+    Example
+    -------
+    @deco_validatelog
+    def Check_ExampleValiDef(self, *checkDataList):
+        @deco_runvalicheck(self.Get_CheckDataList(*checkDataList))
+        async def check_validate(dataName):
+            _errCount = 0
+
+            # Validate Check Code.
+
+            return _errCount
+        return check_validate
+    """
     def __init__(self):
         super().__init__()
-        self._validationDir = self.Init_ValidationConfig_Dir()
-
-
-    @deco_usedirmethod(EDirectory.validationDirectory)
-    def Init_ValidationConfig_Dir(self, yamlDir):
-        return yamlDir
 
 
     @deco_validatelog
     def Check_RefValue(self, *checkDataList):
         _errCount = 0
+
+        
 
         return _errCount
 
@@ -25,3 +37,34 @@ class ConfigValidate(Validate):
         _errCount = 0
 
         return _errCount
+
+
+def deco_valiconfigsplit(configType):
+    def get_valiDataNColumn(configType):
+        assert type(configType) == EValidationConfigType, f'err : wrong config tpye input. (input data type : {type(configType)})'
+
+        _checkDict : dict = Get_ConifgFromYaml(configType)
+
+        _parantKeyList = []
+        _parantValueList = []
+
+        _childKeyList = []
+        _childValueList = []
+
+        for (key, value) in _checkDict.items():
+            _parantKeyList.append(str(key).split('.')[0])
+            _parantValueList.append(str(key).split('.')[1])
+            
+            _childKeyList.append(str(value).split('.')[0])
+            _childValueList.append(str(value).split('.')[1])
+        
+        return (_parantKeyList, _parantValueList, _childKeyList, _childValueList)
+
+    def decorator(validate_func):
+        def wrap():
+            (_parantKeyList, _parantValueList, _childKeyList, _childValueList) = get_valiDataNColumn(configType)
+
+            return validate_func()
+
+        return wrap
+    return decorator
