f661a74d08129a445c47bc47587ea2a5ca5e7a32
Star001 <75716022+Star001-KR@users.noreply.github.com>
2025-07-01 20:37:07 +0900

    [김태형] gui 함수 기능 구현

--- FileConverter/Package/Gui/gui_func.py
+++ FileConverter/Package/Gui/gui_func.py
@@ -12,3 +12,29 @@ def press_btn_runAll():
     
     Write_Log(ELogTpye.normal, '----------------------------------------------------------------')
     Write_Log(ELogTpye.normal, 'Complete Run All.')
+
+
+def press_btn_fileConvert(checkExcelDict):
+     assert (type(checkExcelDict) == dict), 'err : wrong data type input.'
+
+     if (Check_AllValidate()):
+          convert = Convert()
+          excelNameList = []
+
+          for key, value in checkExcelDict.items():
+               if value.get() == 1:
+                    excelNameList.append(key)
+
+          for excelName in excelNameList:
+               convert.Convert_ExcelToJson(excelName)
+
+     Write_Log(ELogTpye.normal, '----------------------------------------------------------------')
+     Write_Log(ELogTpye.normal, 'Complete Run.')
+
+
+def press_btn_validateCheck():
+     if (Check_AllValidate()):
+          pass
+     
+     Write_Log(ELogTpye.normal, '----------------------------------------------------------------')
+     Write_Log(ELogTpye.normal, 'Complete Validate Check.')
